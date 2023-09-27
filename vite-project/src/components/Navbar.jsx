0f1ec9b775523e7820a223dcd4821fcabae40c32
Marcelina Acosta <acostamarcelina392@gmail.com>
2023-09-27 08:43:16 -0300

    actualizacion del navbar

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -12,19 +12,46 @@ function Navbar() {
       </div>
       <CartWidget />
       <div className='List-nav'>
-        <ul>
+        <ul className='Ul-container'>
             <li><a href="">INICIO</a></li>
-            <li><a href="">SHOP</a></li>
-            <li><a href="">MEN</a></li>
-            <li><a href="">WOMEN</a></li>
-            <li><a href="">STORES</a></li>
+            <li>
+              <a href="">SHOP</a>
+              <ul className='sub-menu-shop'>
+						    <li><a href="">Submenu1</a></li>
+						    <li><a href="">Submenu2</a></li>
+						    <li><a href="">Submenu3</a></li>
+						    <li><a href="">Submenu4</a></li>
+						  </ul>
+            </li>
+            <li>
+              <a href="">CHILDRENS</a>
+              <ul className='sub-menu-childrens'>
+						          <li><a href="">Submenu1</a></li>
+						          <li><a href="">Submenu2</a></li>
+						          <li><a href="">Submenu3</a></li>
+						          <li><a href="">Submenu4</a></li>
+						    </ul>
+            </li>
+            <li>
+              <a href="">STORES</a>
+              <ul className='sub-menu-stores'>
+						        <li><a href="">Submenu1</a></li>
+						        <li><a href="">Submenu2</a></li>
+						        <li><a href="">Submenu3</a></li>
+						        <li><a href="">Submenu4</a></li>
+						  </ul>
+            </li>
             <li><a href="">NEW</a></li>
-            <li><a href="">HELP</a></li>
+            <li><a href="">HELP</a>
+              <ul className='sub-menu-help'>
+						      <li><a href="">Submenu1</a></li>
+						      <li><a href="">Submenu2</a></li>
+						      <li><a href="">Submenu3</a></li>
+						      <li><a href="">Submenu4</a></li>
+						  </ul>
+            </li>
        </ul>
      </div>
-      
-      
-      <a href=""></a>
       
     </nav>
   );
