54ad954edcbf484390f5a1016757eda65d61c3e7
Marcelina Acosta <acostamarcelina392@gmail.com>
2023-11-29 11:02:54 -0300

    trabaje en el carrito

--- vite-project/src/components/Carrito.jsx
+++ vite-project/src/components/Carrito.jsx
@@ -1,26 +1,45 @@
 import React from 'react'
 import { useContext } from 'react'
 import { context } from '../providers/CustomProvider'
+import "../style/app.scss"
 
 const Carrito = () => {
-  const { carrito } = useContext(context);
+  const { carrito, PrecioTotal, vaciarCarrito, eliminarProducto } = useContext(context);
 
   if (carrito.length === 0) {
     return <p>El carrito está vacío.</p>;
   }
-  console.log('Carrito renderizado:', carrito);
+  
+  const handleVaciar = () => {
+    vaciarCarrito();
+    
+    window.location.reload(); // Recarga la página actual para actualizar el estado del carrito.
+    alert("Carrito vaciado");
+    return; // Necesario para evitar que se ejecute el resto del código.
+  }
+  const handleEliminar = (id) => {  
+   eliminarProducto(id);  
+  }
+  
+  
 
   return (
     <div>
       <h1>carrito </h1>
     {carrito.map((producto) => (
-      <div key={producto.id}>
-        <img src={producto.img} alt={producto.title} className="imgCard"/>
+      <div key={producto.id} className='ContainerCardCart'>
+        <img src={producto.img} alt={producto.title} className="imgCardCart"/>
         <h1>{producto.title}</h1>
         <p>{producto.description}</p>
-        <p>Precio: ${producto.price}</p>
+        <p>Precio unitario : ${producto.price}</p>
+        <p>Cantidad : {producto.cantidad}</p>
+        <p>Precio por cantidad : ${producto.price * producto.cantidad}</p>
+        <button onClick={() => handleEliminar(producto.id)}>Eliminar</button>
       </div>
     ))}
+    <h2>Precio Total : $ {PrecioTotal()}</h2>
+    <button onClick={handleVaciar}>Vaciar Carrito </button>
+    <button>Finalizar Compra </button>
   </div>
   )
 }
--- vite-project/src/providers/CustomProvider.jsx
+++ vite-project/src/providers/CustomProvider.jsx
@@ -13,8 +13,16 @@ function CustomProvider(props) {
     }
     const agregarAlCarrito = (producto) => {
       setCarrito([...carrito, producto]);
-      // setCantidadSeleccionada(cantidad + producto.cantidad);
       setCantidadSeleccionada(carrito.length + 1); 
+    }
+    const PrecioTotal = () => {
+      return carrito.reduce((acc, producto) => acc + producto.price * producto.cantidad, 0);
+    }
+    const vaciarCarrito = () => { 
+      setCarrito([]);
+    }
+    const eliminarProducto = (id) => { 
+      setCarrito(carrito.filter(producto => producto.id !== id));
     }
 
     const valorDelContexto = {
@@ -23,7 +31,10 @@ function CustomProvider(props) {
       setCantidadSeleccionada : setCantidadSeleccionada,
       carrito : carrito,
       setCarrito : setCarrito,
-      agregarAlCarrito : agregarAlCarrito
+      agregarAlCarrito : agregarAlCarrito,
+      PrecioTotal : PrecioTotal,
+      vaciarCarrito : vaciarCarrito,
+      eliminarProducto : eliminarProducto,
   };
 
   return (
